1c5ddfe872a386a8d844bb13e833f16ce7bf409e
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-09-19 16:22:52 -0400

    Updates

---
+++ precache-manifest.6ff0525d3746ec9df31cb2ca346ad866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ff3ac27c6c9619b8bd3308d315d96a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "102468f240d0026e353c",
+    "url": "/static/css/main.187b826e.chunk.css"
+  },
+  {
+    "revision": "12dc07b0a1ce2e6e6d7d",
+    "url": "/static/js/2.e3bf7f8f.chunk.js"
+  },
+  {
+    "revision": "1a26262878d6c8109df89ef545060b59",
+    "url": "/static/js/2.e3bf7f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102468f240d0026e353c",
+    "url": "/static/js/main.3043489f.chunk.js"
+  },
+  {
+    "revision": "ca9a6909d01765a0d7cd",
+    "url": "/static/js/runtime-main.956d8eca.js"
+  }
+]);
